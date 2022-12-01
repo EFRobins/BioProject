a31a777494cbd517480511c152ead00288f329eb
PPPPPPPPeter <65163220+PPPPPPPPeter@users.noreply.github.com>
2022-12-01 15:31:01 +0000

    Update task2 as final submited verison

--- capyle-master/ca_descriptions/task2_TimeWindDirect.py
+++ capyle-master/ca_descriptions/task2_TimeWindDirect.py
@@ -25,7 +25,7 @@ def setup(args):
     config_path = args[0]
     config = utils.load(config_path)
     # -- THE CA MUST BE RELOADED IN THE GUI IF ANY OF THE BELOW ARE CHANGED --
-    config.title = "Probability_applied"
+    config.title = "Task2_ChangeWindDir"
     config.dimensions = 2
 
     # State 0: chaparral
@@ -37,14 +37,14 @@ def setup(args):
     # State 6: yellowy orange 1st stage burning
     # Stage 7: orange
 
-    config.states = (0, 1, 2, 3, 4, 5, 6, 7, 8)
+    config.states = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
     config.wrap = False
     # -------------------------------------------------------------------------
 
     # ---- Override the defaults below (these may be changed at anytime) ----
 
     config.state_colors = [(0.7, 0.7, 0.2), (0.4, 0.8, 1), (0.3, 0.4, 0),
-                           (0.9, 1, 0), (0.7, 0, 0.1), (0.4, 0.4, 0.4), (1, 0.9, 0.2), (1, 0.6, 0.1), (0, 0, 0)]
+                           (0.9, 1, 0), (0.7, 0, 0.1), (0.4, 0.4, 0.4), (1, 0.9, 0.2), (1, 0.6, 0.1), (0, 0, 0), (0.4, 0.3, 0)]
 
     global geneNum
     config.num_generations = 1000
@@ -69,7 +69,7 @@ def setup(args):
     gridray[80:140, 0: 100] = 2
 
     # create the part of scrubland in canyon
-    gridray[20:140, 120:140] = 3
+    gridray[20:140, 120:130] = 3
 
     # create the part of lake :
     gridray[70:80, 20:100] = 1
@@ -78,6 +78,8 @@ def setup(args):
     gridray[175:185, 75:85] = 8
 
 
+
+    # Start firing on incinerator here:
     for i in range(0, 50):
         x = random.randint(189, 199)
         y = random.randint(0, 10)
@@ -97,115 +99,120 @@ timeTrack = np.zeros((200, 200))
 t = 0
 run_once = 0
 
+# change different wind direction, but this file default is "north" here.
+# windDirection = "north"
+
+windDirection = "south"
+# windDirection = "east"
+# windDirection = "west"
+# windDirection = "northwest"
+# windDirection = "northeast"
+# windDirection = "southwest"
+# windDirection = "southeast"
+
 
 def transition_function(grid, neighbourstates, neighbourcounts):
     """Function to apply the transition rules
     and return the new grid"""
     # YOUR CODE HERE
 
-    burning = (grid == 4)
-
     updateBurn(grid, neighbourstates, neighbourcounts)
 
     return grid
 
 
-# the function of firing chaparall simulation:
+# the function of generate the probability of firing and change different wind directions,
+# will be invoked in @updateBurn() function.:
 
-def generateProbability(grid, diff_windDir, burningNeighbourCount):
-    probability_0 = np.where(grid == 0, 0.1 * burningNeighbourCount, 0)
-    probability_2 = np.where(grid == 2, 0.02 * burningNeighbourCount, 0)
-    probability_3 = np.where(grid == 3, 0.4 * burningNeighbourCount, 0)
-    probability_all = probability_0 + probability_2 + probability_3
-    probability_all_NW = np.where(diff_windDir, probability_all * 1.2, probability_all)
+def generateProbability(grid, neighbourstates, burningNeighbourCount):
+    probability_0 = np.where(grid == 0, 0.07 * burningNeighbourCount, 0)
+    probability_2 = np.where(grid == 2, 0.0175 * burningNeighbourCount, 0)
+    probability_3 = np.where(grid == 3, 0.6 * burningNeighbourCount, 0)
+    probability_9 = np.where(grid == 9, 0.003 * burningNeighbourCount, 0)
+    probability_all = probability_0 + probability_2 + probability_3 + probability_9
 
-    return probability_all_NW
-
-
-def updateBurn(grid, neighbourstates, neighbourcounts):
     # unpack the state arrays
-    # Here to change the wind direction:
     NW, N, NE, W, E, SW, S, SE = neighbourstates
+    northBurning = (N == 6)
+    southBurning = (S == 6)
+    eastBurning = (E == 6)
+    westBurning = (W == 6)
+    northwestBurning = (NW == 6)
+    northeastBurning = (NE == 6)
+    southwestBurning = (SW == 6)
+    southeastBurning = (SE == 6)
 
-    # array of pixels with a northern pixel that is burning
-    # Here to change the wind direction:
+    # different wind direction, but this file default is "north".
 
-    # --------------------------------------------------north wind:
-    diff_windDir = (N == 6) | (NW == 6) | (NE == 6)
-    # --------------------------------------------------south wind:
-    # diff_windDir = (S == 6) | (SW == 6) | (SE == 6)
-    # --------------------------------------------------West wind:
-    # diff_windDir = (W == 6) | (NW == 6) | (SW == 6)
-    # --------------------------------------------------East wind:
-    # diff_windDir = (E == 6) | (NE == 6) | (SE == 6)
-    # --------------------------------------------------North West wind:
-    # diff_windDir = (N == 6) | (NW == 6) | (W == 6)
-    # --------------------------------------------------South West wind:
-    # diff_windDir = (S == 6) | (SW == 6) | (W == 6)
-    # --------------------------------------------------South East wind:
-    # diff_windDir = (S == 6) | (ES == 6) | (E == 6)
-    # --------------------------------------------------North East wind:
-    # diff_windDir = (S == 6) | (ES == 6) | (E == 6)
+    if windDirection == "north":
+        probability_all_W = np.where(northBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(southBurning, probability_all_W * 0.5, probability_all_W)
+
+    elif windDirection == "south":
+        probability_all_W = np.where(southBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(northBurning, probability_all_W * 0.5, probability_all_W)
+
+    elif windDirection == "east":
+        probability_all_W = np.where(eastBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(westBurning, probability_all_W * 0.5, probability_all_W)
+
+    elif windDirection == "west":
+        probability_all_W = np.where(westBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(eastBurning, probability_all_W * 0.2, probability_all_W)
+
+    elif windDirection == "northwest":
+        probability_all_W = np.where(northwestBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(southeastBurning, probability_all_W * 0.5, probability_all_W)
+
+    elif windDirection == "northeast":
+        probability_all_W = np.where(northeastBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(southwestBurning, probability_all_W * 0.5, probability_all_W)
+
+    elif windDirection == "southwest":
+        probability_all_W = np.where(southwestBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(northeastBurning, probability_all_W * 0.5, probability_all_W)
+
+    elif windDirection == "southeast":
+        probability_all_W = np.where(southeastBurning, probability_all * 2.5, probability_all)
+        probability_all_W = np.where(northwestBurning, probability_all_W * 0.5, probability_all_W)
+
+    else:
+        probability_all_W = probability_all
+
+    return probability_all_W
+
+
+# updateBurn function will be invoked in @transition_function().:
+def updateBurn(grid, neighbourstates, neighbourcounts):
+
 
     randomNumber = np.random.rand(200, 200)
-    toBurn = generateProbability(grid, diff_windDir, neighbourcounts[6]) > randomNumber
+    toBurn = generateProbability(grid, neighbourstates, neighbourcounts[6]) > randomNumber
 
     burning = (grid == 6) | (grid == 4) | (grid == 7) | (grid == 5)
 
     itemindex = np.where(burning == True)
     timeTrack[itemindex] += 1
-    # TODO : here is hongyu's time line with proportion:
 
-    #     # A whole timeline = 60days , 30days for firing simulation
-    #     # / another 30 days for reburn simulation
 
-    #     # timeTrack for chaparral:
-    #     # Assume chaparral can be burning for 3 days to 6 days
+    toOrange = ((timeTrack == random.randint(75,85)) & (gridray == 2)) | (
+                (timeTrack == random.randint(1, 2)) & (gridray == 3)) | (
+                       (timeTrack == random.randint(20, 30)) & (gridray == 0)) | (
+                           (timeTrack == random.randint(20, 30)) & (gridray == 6)) | (
+                                (timeTrack == random.randint(20, 30)) & (gridray == 9))
+    toRed = ((timeTrack >= random.randint(250, 256)) & (gridray == 2)) | (
+                (timeTrack >= random.randint(2,3 )) & (gridray == 3)) | (
+                    (timeTrack >= 60) & (gridray == 0)) | ((timeTrack >= random.randint(59,60)) & (gridray == 6)) | (
+                        (timeTrack >= random.randint(59,60)) & (gridray == 9))
+    stopBurn = ((timeTrack >= 720) & (gridray == 2)) | ((timeTrack >= 4) & (gridray == 3)) | (
+            (timeTrack >= 166) & (gridray == 0)) | ((timeTrack >= 166) & (gridray == 6))  | ((timeTrack >= 180) & (gridray == 9))
 
-    #     rangeStart_chap = int(0.05 * geneNum)
-    #     rangeEnd_chap = int(0.1 * geneNum)
-    #     burningTime_chap = random.randint(rangeStart_chap, rangeEnd_chap)
-
-    #     # timeTrack for scrubland:
-    #     # Assume scrubland can be burning for 3 hours to 6 hours
-
-    #     rangeStart_scru = int((0.05 * geneNum) / 24)
-    #     rangeEnd_scru = int((0.1 * geneNum) / 24)
-    #     burningTime_scru = random.randint(rangeStart_scru, rangeEnd_scru)
-
-    #     # timeTrack for dence forest:
-    #     # Assume dense forest can be burning for 20 days to 30 days
-
-    #     rangeStart_forest = int(geneNum / 3)
-    #     rangeEnd_forest = int(0.5 * geneNum)
-    #     burningTime_forest = random.randint(rangeStart_forest, rangeEnd_forest)
-
-    toOrange = ((timeTrack == random.randint(20, 40)) & (gridray == 2)) | (
-                (timeTrack == random.randint(1, 3)) & (gridray == 3)) | (
-                       (timeTrack == random.randint(5, 15)) & (gridray == 0)) | (
-                           (timeTrack == random.randint(5, 15)) & (gridray == 6))
-    toRed = ((timeTrack >= random.randint(50, 70)) & (gridray == 2)) | (
-                (timeTrack >= random.randint(4, 6)) & (gridray == 3)) | (
-                    (timeTrack >= 20) & (gridray == 0)) | ((timeTrack >= random.randint(16, 30)) & (gridray == 6))
-    stopBurn = ((timeTrack >= 150) & (gridray == 2)) | ((timeTrack >= 8) & (gridray == 3)) | (
-            (timeTrack >= 40) & (gridray == 0)) | ((timeTrack >= 40) & (gridray == 6))
-
-    # TODO : here is hongyu's time line with proportion:
-    #     stopBurn = ((timeTrack >= burningTime_forest) & (gridray == 2)) | (
-    #                 (timeTrack >= burningTime_scru) & (gridray == 3)) | \
-    #                ((timeTrack >= burningTime_chap) & (gridray == 0)) | ((timeTrack >= burningTime_chap) & (gridray == 6))
-
-    restoreScrub = ((timeTrack >= 240) & (gridray == 3))
-    restoreChap = ((timeTrack >= 350) & (gridray == 0))
-    restoreForest = ((timeTrack >= 650) & (gridray == 2))
 
     grid[toBurn] = 6
     grid[toOrange] = 7
     grid[toRed] = 4
     grid[stopBurn] = 5
-    grid[restoreScrub] = 3
-    grid[restoreChap] = 0
-    grid[restoreForest] = 2
+
 
     global t
     t += 1
@@ -213,15 +220,10 @@ def updateBurn(grid, neighbourstates, neighbourcounts):
         printTime(t)
 
 
-
-
-
-
 def printTime(t):
     print("The fire reached the town at: ", str(t))
     global run_once
     run_once += 1
-
 
 
 def main():
